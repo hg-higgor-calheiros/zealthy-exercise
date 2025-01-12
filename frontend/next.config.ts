dd7b4c8fdc4805ca18721ec04e8b09f671c8386f
hg-higgor-calheiros <141778712+hg-higgor-calheiros@users.noreply.github.com>
2025-01-12 19:49:21 -0300

    adjust build output

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: 'dist',
+  output: "export",
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
