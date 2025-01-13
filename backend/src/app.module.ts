461a0b8c1da8bf78660ca6e1b876c7283dcb14c3
hg-higgor-calheiros <141778712+hg-higgor-calheiros@users.noreply.github.com>
2025-01-13 10:02:29 -0300

    adding env vars

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { StepsModule } from './steps/steps.module';
 import { Step } from './steps/entities/step.entity';
@@ -10,13 +11,16 @@ import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.MYSQL_HOST || 'localhost',
       port: 3306,
-      username: 'user',
-      password: 'password',
-      database: 'db',
+      username: process.env.MYSQL_USERNAME || 'user',
+      password: process.env.MYSQL_PASSWORD || 'password',
+      database: process.env.MYSQL_DATABASE || 'db',
       entities: [Step, User],
       synchronize: true,
     }),
